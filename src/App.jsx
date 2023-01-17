9f6c425b5280b45c7cf54aee83b3aaf2f04db030
Shlomo Salomon <shlomisal99@gmail.com>
2023-01-17 16:29:35 +0200

    completed the header when loged out

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './style/style.css'
 
 export default function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
-import siteLogo from '../images/images.jpg'
+import siteLogo from '../images/logo.jpeg'
+import genPerson from '../images/gen-person.jpg'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return (
-    <div className="d-flex justify-content-evenly align items-center">
-    <Link to={''} className='col-1'>
-        <img src={siteLogo} alt=""  className="img-fluid " />
-    </Link>
-</div>
+    <Navbar collapseOnSelect expand="lg" dir='rtl' style={{ backgroundColor: "#1c4062" }} className="d-flex justify-content-around align-items-center">
+      <Container>
+        <Link to={''} className='col-lg-1 col-4'>
+          <img src={siteLogo} alt="" className="img-fluid " />
+        </Link>
+        
+      <div className="col-3 position-lg-absolute position-static start-0 d-flex justify-content-center align-items-center">
+        <Link to={'../../signin'}  className=' col-6 col-lg-5 m-2 m-lg-0 p-0 p-lg-5'>
+          <div className=" border rounded-circle  border-white">
+            <img src={genPerson} alt="" className="img-fluid" style={{ mixBlendMode: 'color-burn' }} />
+          </div>
+        </Link>
+        <Link to={'../../signin'} className="d-none d-lg-block text-decoration-none p-1 fs-4 text-white ">כניסה</Link>
+        <span className="d-none d-lg-block text-white fs-4"> / </span>
+        <Link to={'../../signup'} className="d-none d-lg-block text-decoration-none p-1 fs-4 text-white ">הרשמה</Link>
+      </div>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto  position-lg-absolute position-static start-50  col-4 d-lg-flex justify-content-evenly align-items-center">
+              <Link to={''} className='link-light text-decoration-none fs-4'>דף הבית</Link>
+              <Link to={'about'} className='link-light text-decoration-none fs-4'>עלינו</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
