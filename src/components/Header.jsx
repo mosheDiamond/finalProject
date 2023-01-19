6362f7e300d972f6443105cc04b948e3d39a5ff5
Shlomo Salomon <shlomisal99@gmail.com>
2023-01-19 10:46:01 +0200

    final changes to generic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,32 +4,34 @@ import genPerson from '../images/gen-person.jpg'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" dir='rtl' className="d-flex justify-content-around align-items-center bg-dark-blue">
+    <Navbar collapseOnSelect expand="lg" dir='rtl' className="bg-dark-blue">
       <Container>
-        <Link to={''} className='col-lg-1 col-4'>
+        <Link to={''} className='col-lg-2 col-4 '>
           <img src={siteLogo} alt="" className="img-fluid " />
         </Link>
-        
-      <div className="col-3 position-lg-absolute position-static start-0 d-flex justify-content-center align-items-center">
-        <Link to={'../../signin'}  className=' col-6 col-lg-5 m-2 m-lg-0 p-0 p-lg-5'>
-          <div className=" border rounded-circle  border-white">
-            <img src={genPerson} alt="" className="img-fluid" style={{ mixBlendMode: 'color-burn' }} />
-          </div>
-        </Link>
-        <Link to={'../../login'} className="d-none d-lg-block text-decoration-none p-1 fs-4 text-white ">כניסה</Link>
-        <span className="d-none d-lg-block text-white fs-4"> / </span>
-        <Link to={'../../signup'} className="d-none d-lg-block text-decoration-none p-1 fs-4 text-white ">הרשמה</Link>
-      </div>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto  position-lg-absolute position-static start-50  col-4 d-lg-flex justify-content-evenly align-items-center">
-              <Link to={''} className='link-light text-decoration-none fs-4'>דף הבית</Link>
-              <Link to={'about'} className='link-light text-decoration-none fs-4'>עלינו</Link>
+        <Navbar.Collapse id="responsive-navbar-nav ">
+          <Nav className="col-4 d-lg-flex justify-content-evenly align-items-center ">
+            <Link to={'../../'} className='link-light text-decoration-none fs-4'>דף הבית</Link>
+            <Link to={'../../about'} className='link-light text-decoration-none fs-4'>עלינו</Link>
           </Nav>
         </Navbar.Collapse>
+        <Dropdown className="m-3 m-md-0 position-absolute position-lg-static end-50 top-0 col-2 col-md-1">
+            <Dropdown.Toggle className="bg-dark-blue border-0  "  id="dropdown-basic">
+              <div className="col-md-10 border rounded-circle  border-white">
+                <img src={genPerson} alt="" className="img-fluid" style={{ mixBlendMode: 'color-burn' }} />
+              </div>
+            </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item><Link to={'../../login'} className="text-decoration-none p-1 fs-4  ">כניסה</Link></Dropdown.Item>
+            <Dropdown.Item><Link to={'../../signup'} className="text-decoration-none p-1 fs-4  ">הרשמה</Link></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
       </Container>
     </Navbar>
   )
